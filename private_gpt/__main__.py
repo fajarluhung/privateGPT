fd82df7c529c6c5646e6f69ecc8cfd6ae611d652
Fajar Luhung Parasdyo <66538836+fajarluhung@users.noreply.github.com>
2023-11-27 21:18:08 +0700

    Update __main__.py to use gradio launch

--- private_gpt/__main__.py
+++ private_gpt/__main__.py
@@ -8,4 +8,7 @@ from private_gpt.settings.settings import settings
 # Set log_config=None to do not use the uvicorn logging configuration, and
 # use ours instead. For reference, see below:
 # https://github.com/tiangolo/fastapi/discussions/7457#discussioncomment-5141108
-uvicorn.run(app, host="0.0.0.0", port=settings().server.port, log_config=None)
+# uvicorn.run(app, port=settings().server.port, log_config=None)
+_blocks = app.get_ui_blocks()
+_blocks.queue()
+_blocks.launch(share=True, debug=False, show_api=False)
